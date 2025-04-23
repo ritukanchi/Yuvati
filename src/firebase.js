dbd1a12701873f05814e6601e811657c8084eb03
ritukanchi <ritukanchi04@gmail.com>
2025-04-23 11:17:21 +0530

    minor fix

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,16 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {  
-	apiKey: "AIzaSyBXQjXR0vCDVs_vsBii9WHf7Da-UeBMsXo",  
-	authDomain: "yuvati-ca4b0.firebaseapp.com",  
-	projectId: "yuvati-ca4b0",  
-	storageBucket: "yuvati-ca4b0.appspot.com",  
-	messagingSenderId: "762101177289",  
-	appId: "1:762101177289:web:d031dbeaa274c76d496618",  
-	measurementId: "G-THT3K1CHEL"   
-};
+const firebaseConfig = {
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  };
+  
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
